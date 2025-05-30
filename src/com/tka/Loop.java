9aef5d30a4d5ab985eafd079555c1b24d9d6b08f
Aakash-Jcode <aade.a1214@gmail.com>
2025-05-30 17:58:41 +0530

    table five

--- src/com/tka/Loop.java
+++ src/com/tka/Loop.java
@@ -6,6 +6,12 @@ public class Loop {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("2 * " + i + " = " + (2 * i));
 		}
+		
+		}
+		public static void printTable5() {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println("5 * " + i + " = " + (5 * i));
+			}
 	}
 
 }
