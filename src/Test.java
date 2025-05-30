31ce81df9426a100592800300be630bb0166c304
ram-chadar <javaprojectwork2715@gmail.com>
2025-05-30 17:10:24 +0530

    worked on table

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,11 @@
+import com.tka.Loop;
 
 public class Test {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
+		Loop.printTable();
 
 	}
 
---
+++ src/com/tka/Loop.java
@@ -0,0 +1,11 @@
+package com.tka;
+
+public class Loop {
+	
+	public static void printTable() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("2 * " + i + " = " + (2 * i));
+		}
+	}
+
+}
