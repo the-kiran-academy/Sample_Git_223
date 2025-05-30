cc3ff3844f92464af26addad6689053370af26ca
ThePawan2003 <pawanwahule556@gmail.com>
2025-05-31 00:28:22 +0530

    table seven

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,13 @@
 import com.tka.Loop;
 
+
 public class Test {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
 		Loop.printTable();
-		Loop.printTableThree();
+		Loop.printTableSeven();
 
 	}
 
